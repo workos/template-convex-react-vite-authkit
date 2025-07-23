20122d89a53fbff88037080fd5ce1938a7c7fc14
Nick Nisi <nick.nisi@workos.com>
2025-07-23 14:12:55 -0500

    update auth.config.ts to no longer need domain, issuer, or applicationID

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,11 +3,10 @@ const clientId = process.env.WORKOS_CLIENT_ID;
 const authConfig = {
   providers: [
     {
-      // WorkOS JWT configuration
-      // Make sure to configure WORKOS_CLIENT_ID in your
-      // Convex dashboard environment variables
-      domain: `https://api.workos.com/user_management/${clientId}`,
-      applicationID: clientId,
+      type: 'customJwt',
+      issuer: 'https://api.workos.com/',
+      algorithm: 'RS256',
+      jwks: `https://api.workos.com/sso/jwks/${clientId}`,
     },
   ],
 };
