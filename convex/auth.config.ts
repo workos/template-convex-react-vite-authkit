151e01040616d696850a1aceec5fc59cbea182b9
Nick Nisi <nick.nisi@workos.com>
2025-07-28 06:58:55 -0600

    account for both JWT types in auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -7,6 +7,14 @@ const authConfig = {
       issuer: 'https://api.workos.com/',
       algorithm: 'RS256',
       jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+      applicationID: clientId,
+    },
+    {
+      type: 'customJwt',
+      issuer: `https://api.workos.com/user_management/${clientId}`,
+      algorithm: 'RS256',
+      jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+      applicationID: clientId,
     },
   ],
 };
